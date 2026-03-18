6b4d12f10f5b04efa83c1073807df700e6ff2008
Eraxium17 <eraxproy.t@gmail.com>
2026-03-18 19:42:39 +0200

    Update Book.cs

--- WebLibrary/Models/Book.cs
+++ WebLibrary/Models/Book.cs
@@ -3,13 +3,13 @@
     public class Book
     {
         public int BookId { get; set; }
-        public string? Title { get; set; }
-        public string? Isbn { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
         public int PublishedYear { get; set; }
         public int CopiesTotal { get; set; }
-        public ICollection<BookAuthor> BookAuthors { get; set; }
-        public ICollection<BookGenre> BookGenres { get; set; }
-        public ICollection<Loan> Loans { get; set; }
-        public ICollection<Review> Reviews { get; set; }
+        public ICollection<BookAuthor>? BookAuthors { get; set; }
+        public ICollection<BookGenre>? BookGenres { get; set; }
+        public ICollection<Loan>? Loans { get; set; }
+        public ICollection<Review>? Reviews { get; set; }
     }
 }
