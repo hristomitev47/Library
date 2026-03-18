fe2788c3c04c4e49b2622da73318d543340630bb
Eraxium17 <eraxproy.t@gmail.com>
2026-03-18 19:45:45 +0200

    Update Genre.cs

--- WebLibrary/Models/Genre.cs
+++ WebLibrary/Models/Genre.cs
@@ -3,9 +3,9 @@
     public class Genre
     {
         public int GenreId { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
 
-        public ICollection<BookGenre> BookGenres { get; set; }
+        public ICollection<BookGenre>? BookGenres { get; set; }
     }
 }
