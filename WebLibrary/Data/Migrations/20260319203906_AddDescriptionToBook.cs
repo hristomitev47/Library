278fe9637450fbbfc7d26325ee9b54e3b2aa9b81
Eraxium17 <eraxproy.t@gmail.com>
2026-03-20 11:01:01 +0200

    Add files via upload

---
+++ WebLibrary/Data/Migrations/20260319203906_AddDescriptionToBook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Books");
+        }
+    }
+}
