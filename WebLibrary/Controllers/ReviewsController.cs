524cfa3af18b9ce64f9a73170fb808337f1749b3
hristo.dev <hristomitev2008@outlook.com>
2026-03-20 10:42:02 +0200

    Update ReviewsController

--- WebLibrary/Controllers/ReviewsController.cs
+++ WebLibrary/Controllers/ReviewsController.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using WebLibrary.Data;
 using WebLibrary.Models;
 
 namespace WebLibrary.Controllers
 {
-
+    [Authorize]
     public class ReviewsController : Controller
     {
         private readonly ApplicationDbContext _context;
