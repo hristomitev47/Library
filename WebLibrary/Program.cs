37daf0729cd18453bb8df9954fca9633a7a4d594
hristo.dev <hristomitev2008@outlook.com>
2026-03-20 10:48:35 +0200

    Refactor Program to support async Main and role setup
    Refactored Main method to be asynchronous and added role management for Staff and Member. Included user creation logic for staff member with initial setup.

--- WebLibrary/Program.cs
+++ WebLibrary/Program.cs
@@ -6,23 +6,26 @@ namespace WebLibrary
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Identity/Account/Login";
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -30,22 +33,55 @@ namespace WebLibrary
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapStaticAssets();
             app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Books}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                name: "default",
+                pattern: "{controller=Books}/{action=Index}/{id?}");
             app.MapRazorPages()
                .WithStaticAssets();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                string[] roles = { "Staff", "Member" };
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                }
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                var staffEmail = "admin@library.com";
+                var staffUser = await userManager.FindByEmailAsync(staffEmail);
+                if (staffUser == null)
+                {
+                    staffUser = new IdentityUser { UserName = staffEmail, Email = staffEmail, EmailConfirmed = true };
+                    await userManager.CreateAsync(staffUser, "Admin@1234");
+                    await userManager.AddToRoleAsync(staffUser, "Staff");
+
+                    var adminMember = new WebLibrary.Models.Member
+                    {
+                        FirstName = "Hristo",
+                        LastName = "Mitev",
+                        Email = staffEmail,
+                        Phone = "",
+                        MembershipType = "Staff",
+                        BooksRead = 0,
+                        JoinDate = DateTime.Now
+                    };
+                    dbContext.Members.Add(adminMember);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
 
             app.Run();
         }
