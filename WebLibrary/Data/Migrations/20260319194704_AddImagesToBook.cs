20ac3ccefdc33f44a6b4dc55af50749dbe1cca8a
Eraxium17 <eraxproy.t@gmail.com>
2026-03-20 11:00:22 +0200

    Add files via upload

---
+++ WebLibrary/Data/Migrations/20260319194704_AddImagesToBook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagesToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Books");
+        }
+    }
+}
