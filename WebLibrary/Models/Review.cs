a4b26596bdb18fed07e884ad4d90e88040eb8c1c
Eraxium17 <eraxproy.t@gmail.com>
2026-03-20 10:50:59 +0200

    Update Review.cs

--- WebLibrary/Models/Review.cs
+++ WebLibrary/Models/Review.cs
@@ -4,11 +4,11 @@
     {
         public int ReviewId { get; set; }
         public int BookId { get; set; }
-        public Book Book { get; set; }
+        public Book? Book { get; set; }
         public int MemberId { get; set; }
-        public Member Member { get; set; }
-        public string Comment { get; set; }
-        public int Rating { get; set; } 
+        public Member? Member { get; set; }
+        public string? Comment { get; set; }
+        public double Rating { get; set; }  
         public DateTime CreatedAt { get; set; }
     }
 }
